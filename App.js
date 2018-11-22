fbe2e29e5393fa3d4c85244f40b2b6845002ad9d
Res Yeg <shaqk702@gmail.com>
2018-11-22 14:36:38 -0800

    update App and create SearchPage

--- App.js
+++ App.js
@@ -9,7 +9,8 @@
 'use strict';
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, NavigatorIOS} from 'react-native';
+import SearchPage from './SearchPage';
 
 // const instructions = Platform.select({
 //   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -18,8 +19,8 @@ import {Platform, StyleSheet, Text, View} from 'react-native';
 //     'Shake or press menu button for dev menu',
 // });
 
-type Props = {};
-export default class App extends Component<Props> {
+// type Props = {};
+// export default class App extends Component<Props> {
   // render() {
   //   return (
   //     <View style={styles.container}>
@@ -29,9 +30,24 @@ export default class App extends Component<Props> {
   //     </View>
   //   );
   // }
+  // render() {
+    // return React.createElement(Text, {style: styles.description}, "Search for houses to buy!");
+    // return <Text style={styles.description}>Search for houses to buy! (Again)</Text>;
+    // return <Text style={styles.description}>Search for houses to buy!</Text>;
+  // } 
+// }
+
+export default class App extends Component<{}> {
   render() {
-    return React.createElement(Text, {style: styles.description}, "Search for houses to buy!");
-  }  
+    return (
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Property Finder',
+          component: SearchPage,
+        }}/>
+    );
+  }
 }
 
 // const styles = StyleSheet.create({
@@ -53,10 +69,7 @@ export default class App extends Component<Props> {
 //   },
 // });
 const styles = StyleSheet.create({
-  description: {
-    fontSize: 18,
-    textAlign: 'center',
-    color: '#656565',
-    marginTop: 65,
-  },
+  container: {
+    flex: 1,
+  },  
 });
