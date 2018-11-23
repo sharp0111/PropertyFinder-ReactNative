2a02e8e57d6de59ea314319bbab71a8e6ce63c7e
Res Yeg <shaqk702@gmail.com>
2018-11-22 16:37:49 -0800

    add styles

--- SearchResults.js
+++ SearchResults.js
@@ -10,19 +10,59 @@ import {
   Text,
 } from 'react-native';
 
+class ListItem extends React.PureComponent {
+    _onPress = () => {
+      this.props.onPressItem(this.props.index);
+    }
+  
+    render() {
+      const item = this.props.item;
+      const price = item.price_formatted.split(' ')[0];
+      return (
+        <TouchableHighlight
+          onPress={this._onPress}
+          underlayColor='#dddddd'>
+          <View>
+            <View style={styles.rowContainer}>
+              <Image style={styles.thumb} source={{ uri: item.img_url }} />
+              <View style={styles.textContainer}>
+                <Text style={styles.price}>{price}</Text>
+                <Text style={styles.title}
+                  numberOfLines={1}>{item.title}</Text>
+              </View>
+            </View>
+            <View style={styles.separator}/>
+          </View>
+        </TouchableHighlight>
+      );
+    }
+}  
+
 export default class SearchResults extends Component<{}> {
     _keyExtractor = (item, index) => index;
   
-    _renderItem = ({item}) => {
-        return (
-        <TouchableHighlight
-            underlayColor='#dddddd'>
-            <View>
-            <Text>{item.title}</Text>
-            </View>
-        </TouchableHighlight>
-        );
+    // _renderItem = ({item}) => {
+    //     return (
+    //     <TouchableHighlight
+    //         underlayColor='#dddddd'>
+    //         <View>
+    //         <Text>{item.title}</Text>
+    //         </View>
+    //     </TouchableHighlight>
+    //     );
+    // };
+    _renderItem = ({item, index}) => (
+        <ListItem
+            item={item}
+            index={index}
+            onPressItem={this._onPressItem}
+        />
+    );
+      
+    _onPressItem = (index) => {
+        console.log("Pressed row: "+index);
     };
+      
   
     render() {
         return (
@@ -34,4 +74,32 @@ export default class SearchResults extends Component<{}> {
         );
     }
 }
+
+const styles = StyleSheet.create({
+    thumb: {
+      width: 80,
+      height: 80,
+      marginRight: 10
+    },
+    textContainer: {
+      flex: 1
+    },
+    separator: {
+      height: 1,
+      backgroundColor: '#dddddd'
+    },
+    price: {
+      fontSize: 25,
+      fontWeight: 'bold',
+      color: '#48BBEC'
+    },
+    title: {
+      fontSize: 20,
+      color: '#656565'
+    },
+    rowContainer: {
+      flexDirection: 'row',
+      padding: 10
+    },
+});
   
